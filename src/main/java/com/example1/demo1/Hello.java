41961e4bd52752894d288960a4990826f9079df9
HARI <t.harikrishna1989@gmail.com>
2022-02-05 21:29:05 +0530

    add

---
+++ src/main/java/com/example1/demo1/Hello.java
@@ -0,0 +1,8 @@
+package com.example1.demo1;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("world");
+    }
+}
